47511ae240510783d396e32d56463d8fbf910b34
Stefan Tan <stan3@stuy.edu>
2019-02-03 00:38:35 -0500

    worked on 02_canvas

--- 02_canvas/dotconn.js
+++ 02_canvas/dotconn.js
@@ -1,53 +1,32 @@
-//Stefan Tan
+//Team jamSandwich: Derek Song & Stefan Tan
 //SoftDev2 pd8
-//K00 -- I See a Red Door...
-//2019-01-30
+//K02 -- Connecting the Dots
+//2019-02-01
 
-var c = document.getElementById("slate");
+var c = document.getElementById("playground");
 var ctx = c.getContext("2d");
 
 var clrbut = document.getElementById("clear");
-var togbut = document.getElementById("toggle");
-
-var isDot = false;
 
 var clear = function() {
     ctx.clearRect(0, 0, c.width, c.height);
-}
-
-var toggle = function() {
-    isDot = !isDot;
-    if (isDot) {
-	togbut.innerHTML = "Click for Boxes!";
-    }
-    else {
-	togbut.innerHTML = "Click for Dots!";
-    }
-}
-
-var box = function(x, y) {   
-    ctx.fillStyle = "#ff0000";
-    ctx.fillRect(x,y,100,200);
+    ctx.beginPath();
 }
 
 var dot = function(x, y) {
-    ctx.beginPath();
-    ctx.fillStyle = "blue";
-    ctx.ellipse(x,y,5,5,0,0,2 * Math.PI);
+    ctx.fillStyle = "#FF0000";
+    ctx.arc(x,y,7,0,2 * Math.PI);
     ctx.fill();
+    ctx.moveTo(x, y);
+    //ctx.lineTo(x, y);
+    ctx.stroke();
 }
 
 var draw = function(e) {
-    var x = e.offsetX;
-    var y = e.offsetY;
-    if(isDot) {
-	dot(x,y);
-    }
-    else {
-	box(x,y);
-    }
-}   
+    var x = e.offsetX; //The value of the x coordinate relative to the canvas (upper left corner)
+    var y = e.offsetY; //The value of the y coordinate relative to the canvas (upper left corner)
+    dot(x,y);
+}
 
 clrbut.addEventListener('click', clear);
-togbut.addEventListener('click', toggle);
 c.addEventListener('click', draw);
