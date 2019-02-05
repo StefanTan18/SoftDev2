e6e963179576b2ff6ad69ba0710acc4d4375dd62
Stefan Tan <stan3@stuy.edu>
2019-02-05 18:43:00 -0500

    added 03_canvas-anim

---
+++ 03_canvas-anim/animate.js
@@ -0,0 +1,94 @@
+/*
+  team 3Zs - Stefan Tan and Zane Wang
+  SoftDev2 pd8
+  K03 -- They lock us in the tower whenever we get caught
+  2019-02-04
+*/
+
+// instantiate canvas
+var canvas = document.getElementById('playground');
+var ctx = canvas.getContext("2d");
+
+var coords = {'x': canvas.width/2, 'y': canvas.height/2}
+
+//state variables
+var requestID = 0;
+var radius = 0;
+var growing = true;
+var animated = false;
+
+// clears everything in the canvas
+// starts from the origin of the canvas
+var stopIt = function (e) {
+    console.log(requestID);
+    if (requestID > 0) {
+	console.log('Stopping animation');
+	animated = false;
+	window.cancelAnimationFrame(requestID);
+    }
+    /*
+      e.preventDefault()
+      preventDefault prevents a default action from happening if an event is not handled
+      similarly to a conditional
+      We did not come to a consensus on what preventDefault should be used for in this assignment
+    */
+};
+
+// button stops the animation on the canvas now
+var stopButton = document.getElementById('stop');
+stopButton.addEventListener('click', stopIt);
+
+
+// draw dot, with origin at mouse click
+var dot = function (e) {
+    console.log(requestID);
+    var xCord = coords['x'];
+    var yCord = coords['y'];
+    // formatting using back tick string
+    console.log('Starting animation');
+    drawDot(xCord,yCord);
+};
+
+
+// draw dot given x and y coords
+var drawDot = function (x, y){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // drawing an ellipse requires beginPath for the rendering context to
+    // know where to start drawing the arc of the ellipse from
+    ctx.beginPath();
+    // ellipse params are (x, y, radiusX, radiusY, rotation, startAngle, endAngle)
+    // for a full dot, endAngle should be 2pi radians
+    if (growing) {
+	if (radius >= 0) {
+	    ctx.ellipse(x, y, radius, radius, Math.PI/4, 0, 2 * Math.PI);
+	    radius++;
+	}
+	if (radius * 2 >= canvas.width) {
+	    growing = false;
+	}
+    }
+    else {
+	if (radius > 0) {
+	    ctx.ellipse(x, y, radius, radius, Math.PI/4, 0, 2 * Math.PI);
+	    radius--;
+	}
+	if (radius <= 0) {
+	    growing = true;
+	}
+    }	    	
+    
+    // end with the fill method to fill the arc with the current fill color
+    ctx.fill();
+
+    requestID = window.requestAnimationFrame(dot);
+};
+
+// button starts the animation on the canvas
+var startButton = document.getElementById('circle');
+startButton.addEventListener('click', function(e) {
+    if (requestID == 0 || animated == false) {
+	requestID = window.requestAnimationFrame(dot);
+	animated = true;
+    }
+});
+    
