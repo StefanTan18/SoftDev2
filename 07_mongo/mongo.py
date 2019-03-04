43095752758f8c466823ef97a528b9c6485fe90d
Stefan Tan <stan3@stuy.edu>
2019-03-04 14:19:42 -0500

    updated files

--- 07_mongo/mongo.py
+++ 07_mongo/mongo.py
@@ -8,43 +8,17 @@ import pymongo
 SERVER_ADDR = "104.248.49.56"
 connection = pymongo.MongoClient(SERVER_ADDR)
 db = connection.MongoTrees
-collection = db.earthquakes
+collection = db.prizes
 
 '''
 Finds all earthquakes by alert.
 '''
-def findEQAlert(alert):
-    earthquakes = collection.find({"features.properties.alert":alert})
-    for e in earthquakes:
-        print(e)
+def findPrizeYear(year):
+    prizes = collection.find({"year":year})
+    for p in prizes:
+        print(p)
 
-'''
-Finds all earthquakes with the magnitude.
-'''
-def findEQMag(magnitude):
-    earthquakes = collection.find({"features.properties.mag":magnitude})
-    for e in earthquakes:
-        print(e)
-
-'''
-Finds all earthquakes with n numbers of tsunami.
-'''
-def findEQTsunami(n):
-    earthquakes = collection.find({"features.properties.tsunami":n})
-    for e in earthquakes:
-        print(e)
-
-'''
-Finds all earthquakes with a magnitude less than or equal to the parameter.
-'''
-def findEQMagLess(magnitude):
-    earthquakes = collection.find({"features.properties.mag":{"$lte":score}})
-    for e in earthquakes:
-        print(e)
-
-print("START")
-findEQAlert("green")
-print("HELLO")
+findPrizeYear("2018")
 
 
     
