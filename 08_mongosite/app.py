5feec5ee9170694c2ff634cc8a0d422fd3429f5d
Stefan Tan <stan3@stuy.edu>
2019-03-07 22:20:49 -0500

    updated files

--- 08_mongosite/app.py
+++ 08_mongosite/app.py
@@ -1,25 +1,42 @@
-'''
-Stefan Tan 
-SoftDev1 pd6
-K13 -- Echo Echo Echo
-2018-09-27
-'''
+from flask import Flask, render_template, request, session, url_for, redirect, flash
+import os
+import pymongo
+import json
 
-from flask import Flask, render_template, request
+app = Flask(__name__)
+app.secret_key = os.urandom(32)
 
-app= Flask(__name__) #create instance of class Flask
+SERVER_ADDR = "104.248.49.56"
+connection = pymongo.MongoClient(SERVER_ADDR)
+connection.drop_database("WorldWarZ")
+db = connection.WorldWarZ
+collection = db.movies
+
+f = open("data/movies.json")
+data = json.load(f)
+collection.remove()
+collection.insert_many(data["movies"])
 
 @app.route("/")
-def hello_world():
-    print(app)
-    return render_template("welcome.html")
+def index():
+    return render_template("home.html")
 
-@app.route("/auth")
-def authenticate():
-    print(request)
-    print(request.args)
-    print(request.headers)
-    return render_template('auth.html', username = request.args['username'], method = request.method)
+@app.route("/ip", methods=['POST'])
+def connect():
+    try:
+        connection.drop_database("WorldWarZ")
+        connection = pymongo.MongoClient(request.form['ip'])
+        db = connection.WorldWarZ
+        collection = db.movies
+
+        f = open("./data/movies.json")
+        data = json.load(f)
+        collection.remove()
+        collection.insert_many(data["movies"])
+        
+    except:
+        flash("Not a valid ip address to connect to")
+        print("Not a valid ip address to connect to")
 
 if __name__ == "__main__":
     app.debug = True
